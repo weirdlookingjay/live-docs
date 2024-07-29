b55d33508d70cbbb482c84558e4bb3cc5ccc8669
Joe Clark <weirdlookingjay@live.com>
2024-07-29 12:38:32 -0400

    added clerk auth, header nav, document room route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,45 @@
 import { Inter as FontSans } from "next/font/google"
 
-import "./globals.css"
 import { cn } from "@/lib/utils"
+import './globals.css'
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export const metadatas: Metadata = {
-  title: "LiveDocs",
-  description: "Your go-to collaborative editor"
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default Home
